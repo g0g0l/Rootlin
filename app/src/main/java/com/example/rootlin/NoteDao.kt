1cc7a986fdf1719c4139b305f1cbdcaff776053c
g0g0l <bibaswann.mail@gmail.com>
2019-10-03 19:05:31 +0530

    Minor refactor 1

--- app/src/main/java/com/example/rootlin/NoteDao.kt
+++ app/src/main/java/com/example/rootlin/NoteDao.kt
@@ -3,15 +3,6 @@ package com.example.rootlin
 import androidx.lifecycle.LiveData
 import androidx.room.*
 
-/**
- * The Room Magic is in this file, where you map a Java method call to an SQL query.
- *
- * When you are using complex data types, such as Date, you have to also supply type converters.
- * To keep this example basic, no types that require type converters are used.
- * See the documentation at
- * https://developer.android.com/topic/libraries/architecture/room.html#type-converters
- */
-
 @Dao
 interface NoteDao {
 
